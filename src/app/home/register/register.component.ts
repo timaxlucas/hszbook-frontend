6ac8ba131e58fa7293bb0f485b0a6b9c807dab29
Maximilian Lucas <dev@maximilian-lucas.de>
2019-05-27 16:01:39 +0200

    🚹🚺 added gender support

--- src/app/home/register/register.component.ts
+++ src/app/home/register/register.component.ts
@@ -34,6 +34,7 @@ export class RegisterComponent implements OnInit {
     this.registerForm = this.formBuilder.group({
       firstName: ['', Validators.required],
       lastName: ['', Validators.required],
+      gender: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       street: ['', [Validators.required, Validators.minLength(6)]],
       city: ['', Validators.required],
