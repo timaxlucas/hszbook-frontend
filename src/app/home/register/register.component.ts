66c1e439cd34a449486e861b2feadfae0fe5de91
Maximilian Lucas <dev@maximilian-lucas.de>
2019-07-11 00:00:35 +0200

    credential save fix

--- src/app/home/register/register.component.ts
+++ src/app/home/register/register.component.ts
@@ -36,8 +36,11 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
     this.userService.getDefaultData().pipe(first()).subscribe((res: any) => {
-      if (res === null)
+      if (res === null) {
+        this.detectChanges();
         return;
+      }
+
       const c = this.registerForm.controls;
       c.firstName.setValue(res.firstname);
       c.lastName.setValue(res.surname);
